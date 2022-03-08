a1414b50719752ff27a68683aad00ff9dea2d5f7
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2022-03-08 15:01:59 -0300

    Criando a classe FragmentUtil

--- app/src/main/java/br/com/deeper/MainActivity.kt
+++ app/src/main/java/br/com/deeper/MainActivity.kt
@@ -1,26 +1,15 @@
 package br.com.deeper
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import br.com.deeper.ui.home.HomeFragment
+import br.com.deeper.util.FragmentUtil
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        FragmentUtil.changeFragment(R.id.nav_host_fragment, HomeFragment(), getSupportFragmentManager(), false, null)
     }
 }
